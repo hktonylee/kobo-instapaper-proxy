fec4c63eee1d39d7024a58c49958f2fed28b9e78
hktonylee <iamtonylee@gmail.com>
2025-12-01 18:54:39 -0800

    Adjust navigation timeout for Lambda runtime

--- lambda/src/utils/browser.js
+++ lambda/src/utils/browser.js
@@ -1,5 +1,8 @@
 import { DEFAULT_USER_AGENT } from './constants.js';
 
+const LAMBDA_TIMEOUT_MS = 15000;
+const NAVIGATION_TIMEOUT_MS = LAMBDA_TIMEOUT_MS - 1000;
+
 export const applyStealthTweaks = async (page) => {
   await page.setUserAgent(DEFAULT_USER_AGENT);
   await page.setExtraHTTPHeaders({ 'Accept-Language': 'en-US,en;q=0.9' });
@@ -33,6 +36,7 @@ export const withPage = async (chromiumLib, puppeteerLib, work) => {
 
   try {
     const page = await browser.newPage();
+    page.setDefaultNavigationTimeout(NAVIGATION_TIMEOUT_MS);
     await applyStealthTweaks(page);
     return await work(page);
   } finally {
